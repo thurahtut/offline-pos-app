4110ca80d063b1665d629d87fcf185a1911a63e5
MayYaminThant-NovaSevenTech <mayyaminthant@novaseventech.com>
2024-01-09 16:46:47 +0630

    customer list search

--- lib/view/customer/customer_list_dialog.dart
+++ lib/view/customer/customer_list_dialog.dart
@@ -4,142 +4,22 @@ class CustomerListDialog {
   static Future<Object?> customerListDialogWidget(
     BuildContext mainContext,
   ) {
-    bool isTabletMode = CommonUtils.isTabletMode(mainContext);
-    bool isMobileMode = CommonUtils.isMobileMode(mainContext);
-    double width = MediaQuery.of(mainContext).size.width;
-    double height = MediaQuery.of(mainContext).size.height;
     return CommonUtils.showGeneralDialogWidget(
       mainContext,
       (bContext, anim1, anim2) {
         return AlertDialog(
-          shadowColor: Colors.transparent,
-          backgroundColor: Colors.transparent,
-          contentPadding: EdgeInsets.zero,
-          insetPadding: EdgeInsets.zero,
-          clipBehavior: Clip.antiAliasWithSaveLayer,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(20.0))),
-          content: SingleChildScrollView(
-            child: Container(
-              constraints: BoxConstraints(
-                  maxHeight: height - 100, maxWidth: width - 100),
-              child: Column(
-                mainAxisSize: mainContext.read<CustomerListController>().loading
-                    ? MainAxisSize.min
-                    : MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: width - 100,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Expanded(
-                          child: Row(
-                            children: [
-                              BorderContainer(
-                                text: 'Load Customers',
-                                prefixSvg: 'assets/svg/account_circle.svg',
-                                svgSize: 30,
-                                svgColor: Colors.white,
-                                width: (width -
-                                        100) /
-                                    (isMobileMode
-                                        ? 3
-                                        : isTabletMode
-                                            ? 4
-                                            : 6),
-                                containerColor: Constants.primaryColor,
-                                textColor: Colors.white,
-                                textSize: (isMobileMode
-                                    ? 13
-                                    : isTabletMode
-                                        ? 14
-                                        : 16),
-                                radius: 16,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Row(
-                            children: [
-                              Expanded(child: _searchCustomerWidget()),
-                              Expanded(
-                                child: CommonUtils.okCancelWidget(
-                                  okLabel: 'Create',
-                                  switchBtns: true,
-                                  cancelContainerColor: Colors.white,
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: 10, vertical: 10),
-                                  textSize: 16,
-                                  okCallback: () {
-                                    Navigator.pop(bContext, true);
-                                    CreateCustomerDialog
-                                        .createCustomerDialogWidget(
-                                            mainContext);
-                                  },
-                                  cancelLabel: 'Discard',
-                                  cancelCallback: () {
-                                    Navigator.pop(bContext, false);
-                                  },
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                      padding: EdgeInsets.all(8),
-                      height: height - 200,
-                      width: width - 100,
-                      child: CustomerPaginationTable(
-                        mainContext: mainContext,
-                      )),
-                ],
-              ),
-            ),
-          ),
-        );
+            shadowColor: Colors.transparent,
+            backgroundColor: Colors.transparent,
+            contentPadding: EdgeInsets.zero,
+            insetPadding: EdgeInsets.zero,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20.0))),
+            content: CustomerPaginationTable(
+              mainContext: mainContext,
+              bContext: bContext,
+            ));
       },
-    );
-  }
-
-  static Widget _searchCustomerWidget() {
-    TextEditingController searchCustomerTextController =
-        TextEditingController();
-    return Center(
-      child: TextField(
-        controller: searchCustomerTextController,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: BorderSide(
-              width: 0,
-              style: BorderStyle.none,
-            ),
-          ),
-          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-          hintText: 'Search Customers',
-          hintStyle: TextStyle(fontSize: 14, color: Constants.disableColor),
-          filled: true,
-          fillColor: Constants.greyColor,
-          prefixIcon: UnconstrainedBox(
-            child: SvgPicture.asset(
-              'assets/svg/search.svg',
-              width: 25,
-              height: 25,
-              colorFilter: ColorFilter.mode(
-                Constants.primaryColor,
-                BlendMode.srcIn,
-              ),
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
--- lib/view/customer/customer_pagination_table.dart
+++ lib/view/customer/customer_pagination_table.dart
@@ -3,8 +3,10 @@ import 'dart:math';
 import 'package:offline_pos/components/export_files.dart';
 
 class CustomerPaginationTable extends StatefulWidget {
-  const CustomerPaginationTable({super.key, required this.mainContext});
+  const CustomerPaginationTable(
+      {super.key, required this.mainContext, required this.bContext});
   final BuildContext mainContext;
+  final BuildContext bContext;
 
   @override
   State<CustomerPaginationTable> createState() =>
@@ -32,7 +34,6 @@ class _CustomerPaginationTableState extends State<CustomerPaginationTable> {
       widget.mainContext
           .read<CustomerListController>()
           .resetCustomerListController();
-
       getAllCustomer();
     });
     super.initState();
@@ -51,75 +52,172 @@ class _CustomerPaginationTableState extends State<CustomerPaginationTable> {
 
   Future<void> updateCustomerListToTable() async {
     widget.mainContext.read<CustomerListController>().customerInfoDataSource =
-          CustomerInfoDataSourceForCustomerListScreen(
+        CustomerInfoDataSourceForCustomerListScreen(
       widget.mainContext,
       widget.mainContext.read<CustomerListController>().customerList,
-        passwordTextController,
-        () {},
-        () {
+      passwordTextController,
+      () {},
+      () {
         if (widget.mainContext
             .read<CurrentOrderController>()
             .currentOrderList
             .isEmpty) {
           ScaffoldMessenger.of(widget.mainContext).showSnackBar(SnackBar(
-                content: Text(
-              'There is no order items.',
-              textAlign: TextAlign.center,
-            )));
-            return;
-          }
+              content: Text(
+            'There is no order items.',
+            textAlign: TextAlign.center,
+          )));
+          return;
+        }
         widget.mainContext.read<CurrentOrderController>().isContainCustomer =
             true;
         PasswordDialog.enterPasswordWidget(
                 widget.mainContext, passwordTextController)
-              .then((value) {
-            if (value == true) {
-              Navigator.pushNamed(
+            .then((value) {
+          if (value == true) {
+            Navigator.pushNamed(
               widget.mainContext,
-                OrderPaymentScreen.routeName,
-              );
-              passwordTextController.clear();
-            }
-          });
-        },
+              OrderPaymentScreen.routeName,
+            );
+            passwordTextController.clear();
+          }
+        });
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
     // bool isTabletMode = CommonUtils.isTabletMode(widget.mainContext);
-    return Consumer<CustomerListController>(builder: (_, controller, __) {
-      return controller.loading
-          ? SizedBox(
-              width: 100,
-              height: 100,
-              child: CircularProgressIndicator(
-                color: Colors.white,
+    return _bodyWidget(widget.mainContext);
+  }
+
+  Widget _bodyWidget(BuildContext mainContext) {
+    bool isTabletMode = CommonUtils.isTabletMode(mainContext);
+    bool isMobileMode = CommonUtils.isMobileMode(mainContext);
+    double width = MediaQuery.of(mainContext).size.width;
+    double height = MediaQuery.of(mainContext).size.height;
+    return SingleChildScrollView(
+      child: Container(
+        constraints:
+            BoxConstraints(maxHeight: height - 100, maxWidth: width - 100),
+        child: Column(
+          mainAxisSize: mainContext.watch<CustomerListController>().loading
+              ? MainAxisSize.min
+              : MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: width - 100,
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        BorderContainer(
+                          text: 'Load Customers',
+                          prefixSvg: 'assets/svg/account_circle.svg',
+                          svgSize: 30,
+                          svgColor: Colors.white,
+                          width: (width - 100) /
+                              (isMobileMode
+                                  ? 3
+                                  : isTabletMode
+                                      ? 4
+                                      : 6),
+                          containerColor: Constants.primaryColor,
+                          textColor: Colors.white,
+                          textSize: (isMobileMode
+                              ? 13
+                              : isTabletMode
+                                  ? 14
+                                  : 16),
+                          radius: 16,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(child: _searchCustomerWidget(mainContext)),
+                        Expanded(
+                          child: CommonUtils.okCancelWidget(
+                            okLabel: 'Create',
+                            switchBtns: true,
+                            cancelContainerColor: Colors.white,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 10),
+                            textSize: 16,
+                            okCallback: () {
+                              Navigator.pop(widget.bContext, true);
+                              CreateCustomerDialog.createCustomerDialogWidget(
+                                  mainContext);
+                            },
+                            cancelLabel: 'Discard',
+                            cancelCallback: () {
+                              Navigator.pop(widget.bContext, false);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            )
-          : controller.customerInfoDataSource == null
-              ? SizedBox()
-              : Scrollbar(
-                  controller: scrollController,
-      thumbVisibility: true,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(22),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          physics: AlwaysScrollableScrollPhysics(),
-          child: Container(
-            constraints: BoxConstraints(
-                          maxWidth:
-                              MediaQuery.of(widget.mainContext).size.width,
-                          maxHeight:
-                              MediaQuery.of(widget.mainContext).size.height,
             ),
-                        child: _customerInfoWidget(controller),
-          ),
+            Container(
+                padding: EdgeInsets.all(8),
+                height: height - 200,
+                width: width - 100,
+                child: Consumer<CustomerListController>(
+                    builder: (_, controller, __) {
+                  return controller.loading
+                      ? Container(
+                          constraints: BoxConstraints(
+                              maxHeight: 60,
+                              maxWidth: 60,
+                              minHeight: 60,
+                              minWidth: 60),
+                          child: Center(
+                            child: CircularProgressIndicator(
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      : controller.customerInfoDataSource == null
+                          ? SizedBox()
+                          : Scrollbar(
+                              controller: scrollController,
+                              thumbVisibility: true,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(22),
+                                child: SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  physics: AlwaysScrollableScrollPhysics(),
+                                  child: Container(
+                                    constraints: BoxConstraints(
+                                      maxWidth:
+                                          MediaQuery.of(widget.mainContext)
+                                              .size
+                                              .width,
+                                      maxHeight:
+                                          MediaQuery.of(widget.mainContext)
+                                              .size
+                                              .height,
+                                    ),
+                                    child: _customerInfoWidget(controller),
+                                  ),
+                                ),
+                              ),
+                            );
+                })),
+          ],
         ),
       ),
-                );
-    });
+    );
   }
 
   Stack _customerInfoWidget(CustomerListController controller) {
@@ -130,82 +228,77 @@ class _CustomerPaginationTableState extends State<CustomerPaginationTable> {
           padding: const EdgeInsets.only(bottom: 70.0),
           child: Theme(
             data: Theme.of(widget.mainContext).copyWith(
-                  cardTheme: CardTheme(
-                    elevation: 0,
-                    color: Colors.white,
-                    margin: EdgeInsets.zero,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(22),
-                    ),
-                  ),
-                ),
-                child: PaginatedDataTable2(
-                  dataRowHeight: 65,
-                  headingRowHeight: 70,
-                  dividerThickness: 0.0,
-                  border: TableBorder(
-                      horizontalInside: BorderSide(
-                          color: Constants.disableColor.withOpacity(0.81))),
-                  rowsPerPage: min(
-                    controller.limit,
-                      max(
-                        controller.customerList
-                              .length,
-                          1)),
-              minWidth: MediaQuery.of(widget.mainContext).size.width - 100,
-                  showCheckboxColumn: false,
-                  fit: FlexFit.tight,
-                  hidePaginator: true,
-                  sortColumnIndex: _sortColumnIndex,
-                  sortAscending: _sortAscending ?? false,
-                  headingRowColor: MaterialStateColor.resolveWith(
-                      (states) => Constants.primaryColor),
-                  columns: [
-                    CommonUtils.dataColumn(
-                      // fixedWidth: isTabletMode ? 150 : 120,
-                      text: 'Name',
-                      // onSort: (columnIndex, ascending) =>
-                      //     sort<String>((d) => (d["name"] ?? ''), columnIndex, ascending),
-                    ),
-                    CommonUtils.dataColumn(
-                      fixedWidth: isTabletMode == true ? 300 : 300,
-                      text: 'Address',
-                      // onSort: (columnIndex, ascending) =>
-                      //     sort<String>((d) => (d.name ?? ''), columnIndex, ascending),
-                    ),
-                    CommonUtils.dataColumn(
-                      // fixedWidth: 180,
-                      text: 'Phone',
-                    ),
-                    CommonUtils.dataColumn(
-                      // fixedWidth: isTabletMode ? 180 : null,
-                      text: 'Email',
-                    ),
-                    CommonUtils.dataColumn(
-                      // fixedWidth: 188,
-                      text: 'Discount',
-                    ),
-                    CommonUtils.dataColumn(
-                      fixedWidth: 250,
-                      text: 'Credit',
-                    ),
-                    CommonUtils.dataColumn(
-                      // fixedWidth: 188,
-                      text: 'Credit Limit',
-                    ),
-                    CommonUtils.dataColumn(
-                      // fixedWidth: 188,
-                      text: 'Amount Due',
-                    ),
-                    // CommonUtils.dataColumn(
-                    //   fixedWidth: 100,
-                    //   text: 'action'.tr(),
-                    // ),
-                  ],
-              source: controller
-                      .customerInfoDataSource!,
+              cardTheme: CardTheme(
+                elevation: 0,
+                color: Colors.white,
+                margin: EdgeInsets.zero,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(22),
                 ),
               ),
+            ),
+            child: PaginatedDataTable2(
+              dataRowHeight: 65,
+              headingRowHeight: 70,
+              dividerThickness: 0.0,
+              border: TableBorder(
+                  horizontalInside: BorderSide(
+                      color: Constants.disableColor.withOpacity(0.81))),
+              rowsPerPage:
+                  min(controller.limit, max(controller.customerList.length, 1)),
+              minWidth: MediaQuery.of(widget.mainContext).size.width - 100,
+              showCheckboxColumn: false,
+              fit: FlexFit.tight,
+              hidePaginator: true,
+              sortColumnIndex: _sortColumnIndex,
+              sortAscending: _sortAscending ?? false,
+              headingRowColor: MaterialStateColor.resolveWith(
+                  (states) => Constants.primaryColor),
+              columns: [
+                CommonUtils.dataColumn(
+                  // fixedWidth: isTabletMode ? 150 : 120,
+                  text: 'Name',
+                  // onSort: (columnIndex, ascending) =>
+                  //     sort<String>((d) => (d["name"] ?? ''), columnIndex, ascending),
+                ),
+                CommonUtils.dataColumn(
+                  fixedWidth: isTabletMode == true ? 300 : 300,
+                  text: 'Address',
+                  // onSort: (columnIndex, ascending) =>
+                  //     sort<String>((d) => (d.name ?? ''), columnIndex, ascending),
+                ),
+                CommonUtils.dataColumn(
+                  // fixedWidth: 180,
+                  text: 'Phone',
+                ),
+                CommonUtils.dataColumn(
+                  // fixedWidth: isTabletMode ? 180 : null,
+                  text: 'Email',
+                ),
+                CommonUtils.dataColumn(
+                  // fixedWidth: 188,
+                  text: 'Discount',
+                ),
+                CommonUtils.dataColumn(
+                  fixedWidth: 250,
+                  text: 'Credit',
+                ),
+                CommonUtils.dataColumn(
+                  // fixedWidth: 188,
+                  text: 'Credit Limit',
+                ),
+                CommonUtils.dataColumn(
+                  // fixedWidth: 188,
+                  text: 'Amount Due',
+                ),
+                // CommonUtils.dataColumn(
+                //   fixedWidth: 100,
+                //   text: 'action'.tr(),
+                // ),
+              ],
+              source: controller.customerInfoDataSource!,
+            ),
+          ),
         ),
         _paginationWidget(controller),
       ],
@@ -249,7 +342,48 @@ class _CustomerPaginationTableState extends State<CustomerPaginationTable> {
         ),
       ],
     );
-   
+  }
+
+  Widget _searchCustomerWidget(BuildContext mainContext) {
+    TextEditingController searchCustomerTextController =
+        TextEditingController();
+    return Center(
+      child: TextField(
+        controller: searchCustomerTextController,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide(
+              width: 0,
+              style: BorderStyle.none,
+            ),
+          ),
+          contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+          hintText: 'Search Customers',
+          hintStyle: TextStyle(fontSize: 14, color: Constants.disableColor),
+          filled: true,
+          fillColor: Constants.greyColor,
+          prefixIcon: UnconstrainedBox(
+            child: SvgPicture.asset(
+              'assets/svg/search.svg',
+              width: 25,
+              height: 25,
+              colorFilter: ColorFilter.mode(
+                Constants.primaryColor,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+        ),
+        onEditingComplete: () {
+          mainContext.read<CustomerListController>().filterValue =
+              searchCustomerTextController.text;
+          mainContext.read<CustomerListController>().offset = 0;
+          mainContext.read<CustomerListController>().currentIndex = 1;
+          getAllCustomer();
+        },
+      ),
+    );
   }
 }
 
